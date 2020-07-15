14b7f6dddafdd9f26a42d4a4289bff7f4667e66d
frank9th <ap.teamservices@gmail.com>
2020-07-15 14:36:34 +0100

    added files

--- excel_parser/excel_parser/settings.py
+++ excel_parser/excel_parser/settings.py
@@ -137,18 +137,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-if os.getcwd() == '/excel':
-    import dj_database_url
-    db_from_env = dj_database_url.config(conn_max_age=500)
-    DATABASES['default'].update(db_from_env)
-    #Honor the 'X-forwarded-Proto' header for request.is_secure().
-    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-    #Allow all host headers
-    ALLOWED_HOSTS = ['excelservice.herokuapp.com', '**.herokuapp.com', '127.0.0.1', 'localhost' ]
-    DEBUG = True
-
-    #Static asset configuration
-    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
