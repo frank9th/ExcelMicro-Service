8157e58b17850a3950904451e236869bfb7e4c1b
frank9th <ap.teamservices@gmail.com>
2020-07-15 17:19:17 +0100

    fited settings bug

--- excel_parser/excel_parser/settings.py
+++ excel_parser/excel_parser/settings.py
@@ -128,13 +128,13 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'static'),
 ]
-
+"""
 STATIC_URL = '/staticfiles/'
 STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'staticfiles'),
 ]
 
-
+"""
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
